2fbf7d850be8472ffddbb7faa9e909dfd216a2e1
Branico21 <nicorobayo2110@gmail.com>
2025-04-12 16:06:36 -0500

    Conexion con base de datos rednder

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_secreta_predeterminada')
-    SQLALCHEMY_DATABASE_URI = 'DATABASE_URL'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://parcialagro_user:LMwNYqcp3Tae3HwZrH7wgELuiFoD1lQx@dpg-cvtd0vqdbo4c73cnro4g-a.oregon-postgres.render.com/parcialagro'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
